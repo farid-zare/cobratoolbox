323e99082b48f01c6787240f16d9bdc528b47cad
Farid Zare <farid.zare.edu@gmail.com>
2026-03-06 14:36:53 +0000

    Update comments and formatting in randomObjFBASol.m

--- src/analysis/FBA/randomObjFBASol.m
+++ src/analysis/FBA/randomObjFBASol.m
@@ -9,7 +9,7 @@ function x0 = randomObjFBASol(model, initArgs)
 %    model:        COBRA model structure
 %    initArgs:     Cell array containing the following data:
 %
-%                    1.  osenseStr - Maximize ('max') / minimize ('min')
+%                    1.  osenseStr - Maximize ('max') / Minimize ('min')
 %                    2.  minObjFrac - Minimum initial objective fraction
 %                    3.  minObjValue - Minimum initial objective value (opt)
 %                        (Default = `minObjFrac*sol.f`)
@@ -34,3 +34,4 @@ nRxns = length(model.rxns);
 model.c = rand(nRxns,1)-0.5;
 sol = optimizeCbModel(model,osenseStr);
 x0 = sol.x;
+
